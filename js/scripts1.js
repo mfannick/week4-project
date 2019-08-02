419e21789bc0abdf487757f4510e2c10789f8df2
mfannick <mfannick1@gmail.com>
2019-08-02 15:35:26 +0200

    hiding and showing html contents using buttons click and ppromting and alerting the customer using a button

--- js/scripts1.js
+++ js/scripts1.js
@@ -11,20 +11,83 @@ $(document).ready(function(){
     //     $('#form').toggle();
     //   });
     // })
-    $('#submit1').click(function(){
+
+    // clicking on add to cart the form will come
+    $('#submitCart1').click(function(){
       $('#form').show();
     });
-    $('#submit2').click(function(){
+    $('#submitCart2').click(function(){
         $('#form').show();
       });
-    $('#submit3').click(function(){
+    $('#submitCart3').click(function(){
         $('#form').show();
       });
+
+    // clicking on the order button the order receipt displays
     
-    $('#submit5').click(function(event){
+    $('#submitOrder').click(function(event){
         event.preventDefault(); 
         $('#order').show();
       });
-    //   event.preventDefault(); 
+    
+    // clicking on the Check order amount  button the total amount displays
+    $('#submitDeliver').hide();
+    $('#submitCheck').click(function(event){
+        event.preventDefault(); 
+        $('.total').show();
+        $('#submitDeliver').show();
+      });
+    
+  // clicking on deliver to me the prompt and the alert pops up
+
+  $('#submitDeliver').click(function(event){
+    event.preventDefault();
+    // 1st the prompt comes
+    var location =prompt('Enter your location:')
+    // 2nd the alert comes
+    alert('your order will be delivered to ' + location)
+  });
+
+  // click on another order another form adds itsleft to the later form
+  $('#submitAnother').click(function(event){
+    event.preventDefault(); 
+    $('#form1').append( 
+'<div class="form-group">'+
+        '<label>Pizza size</label>'+
+          '<div class="form-check">'+
+            '<input class="form-check-input" type="radio" name="gridRadios" id="small" value="option1" checked>'+
+            '<label class="form-check-label" for="gridRadios1">Small</label>'+
+          '</div>'+
+          '<div class="form-check">'+
+            '<input class="form-check-input" type="radio" name="gridRadios" id="medium" value="option2">'+
+            '<label class="form-check-label" for="gridRadios2">Medium</label>'+
+          '</div>'+
+          '<div class="form-check">'+
+                '<input class="form-check-input" type="radio" name="gridRadios" id="large" value="option2">'+
+                '<label class="form-check-label" for="gridRadios2">Large</label>'+
+              '</div>'+
+
+'</div>'+
+'<div class="form-group">'+
+        '<label>Pizza crust</label>'+
+        '<select id="crust" class="form-control inputForm">'+
+          '<option selected>Choose pizza crust ...</option>'+
+          '<option>thin </option>'+
+          '<option>thick </option>'+
+          '<option>staffed </option>'+
+        '</select>'+
+'</div>'+
+'<div class="form-group">'+
+        '<label>Pizza toppings</label>'+
+       ' <select id="topping" class="form-control inputForm">'+
+          '<option selected>Choose pizza topping ...</option>'+
+          '<option>Bacon, onion, potato & spinach</option>'+
+          '<option>Beef mince, red onion, mozzarella & BBQ sauce</option>'+
+          '<option>Ham, pineapple & cheese</option>'+
+        '</select>'+
+'</div>'
+    )
+  });
+
     
 })
