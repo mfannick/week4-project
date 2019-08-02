935123723c9c6313f4aea328f2e683041cd120fa
mfannick <mfannick1@gmail.com>
2019-08-02 13:57:03 +0200

    hiding and showing section using jquery through clicking on button

--- js/scripts1.js
+++ js/scripts1.js
@@ -1,3 +1,30 @@
 /************************************FRONT END*************************************/
 
 /**********************************menu index2.html***************************************/
+$(document).ready(function(){
+    
+    // $('#form').hide();
+    // $('#order').hide();
+    // var submits=['submit1','submit2','submit3']
+    // submits.forEach(function(){
+    //    $('#' + submit).click(function(){
+    //     $('#form').toggle();
+    //   });
+    // })
+    $('#submit1').click(function(){
+      $('#form').show();
+    });
+    $('#submit2').click(function(){
+        $('#form').show();
+      });
+    $('#submit3').click(function(){
+        $('#form').show();
+      });
+    
+    $('#submit5').click(function(event){
+        event.preventDefault(); 
+        $('#order').show();
+      });
+    //   event.preventDefault(); 
+    
+})
