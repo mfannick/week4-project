28b9044b1ffeeaea78bfb831dadea7b52c48425c
mfannick <51271889+mfannick@users.noreply.github.com>
2019-08-05 15:57:58 +0200

    Update scripts1.js

--- js/scripts1.js
+++ js/scripts1.js
@@ -33,55 +33,13 @@ function resetField() {
 
 /**********************************menu index2.html***************************************/
 $(document).ready(function () {
-  // click on another order another form adds itsleft to the later form
-  // $('#submitAnother').click(function (event) {
-  //   event.preventDefault();
-  //   $('#form1').append(
-  //     '<div class="form-group">' +
-  //     '<label>Pizza size</label>' +
-  //     '<div class="form-check">' +
-  //     '<input class="form-check-input" type="radio" name="gridRadios" id="small" value="option1" checked>' +
-  //     '<label class="form-check-label" for="gridRadios1">Small</label>' +
-  //     '</div>' +
-  //     '<div class="form-check">' +
-  //     '<input class="form-check-input" type="radio" name="gridRadios" id="medium" value="option2">' +
-  //     '<label class="form-check-label" for="gridRadios2">Medium</label>' +
-  //     '</div>' +
-  //     '<div class="form-check">' +
-  //     '<input class="form-check-input" type="radio" name="gridRadios" id="large" value="option2">' +
-  //     '<label class="form-check-label" for="gridRadios2">Large</label>' +
-  //     '</div>' +
-
-  //     '</div>' +
-  //     '<div class="form-group">' +
-  //     '<label>Pizza crust</label>' +
-  //     '<select id="crust" class="form-control inputForm">' +
-  //     '<option selected>Choose pizza crust ...</option>' +
-  //     '<option value="1">thin </option>' +
-  //     '<option value="2">thick </option>' +
-  //     '<option value="3">staffed </option>' +
-  //     '</select>' +
-  //     '</div>' +
-  //     '<div class="form-group">' +
-  //     '<label>Pizza toppings</label>' +
-  //     ' <select id="topping" class="form-control inputForm">' +
-  //     '<option selected>Choose pizza topping ...</option>' +
-  //     '<option value="1">Bacon, onion, potato & spinach</option>' +
-  //     '<option value="2">Beef mince, red onion, mozzarella & BBQ sauce</option>' +
-  //     '<option value="3">Ham, pineapple & cheese</option>' +
-  //     '</select>' +
-  //     '</div>'
-  //   )
-  // });
+  
   /***************************************************************************************/
 
   // clicking on order button the form will come
   $('#submitCart3').click(function (event) {
     event.preventDefault();
     $('#form').show();
-
-    /***************************************************************************************/
-
     // clicking on the ordersummary button the order receipt displays
 
     $('#submitOrder').last().click(function (event) {
